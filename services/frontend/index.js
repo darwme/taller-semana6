f58753af07b4da5331ba1eb7bc7f16df6b77baef
darwme <darw.mendz@gmail.com>
2025-05-03 21:41:00 -0500

    commit init

---
+++ services/frontend/index.js
@@ -0,0 +1,90 @@
+function newBook(book) {
+    const div = document.createElement('div');
+    div.className = 'column is-4';
+    div.innerHTML = `
+        <div class="card is-shady">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img
+                        src="${book.photo}"
+                        alt="${book.name}"
+                        class="modal-button"
+                    />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="content book" data-id="${book.id}">
+                    <div class="book-meta">
+                        <p class="is-size-4">US$${book.price.toFixed(2)}</p>
+                        <p class="is-size-6">Disponible en stock: 5</p>
+                        <h4 class="is-size-3 title">${book.name}</h4>
+                        <p class="subtitle">${book.author}</p>
+                    </div>
+                    <div class="field has-addons">
+                        <div class="control">
+                            <input class="input" type="text" placeholder="Digite" />
+                        </div>
+                        <div class="control">
+                            <a class="button button-shipping is-info" data-id="${book.id}"> Calcular Flete </a>
+                        </div>
+                    </div>
+                    <button class="button button-buy is-success is-fullwidth">Comprar</button>
+                </div>
+            </div>
+        </div>`;
+    return div;
+}
+
+function calculateShipping(id, cep) {
+    fetch('http://localhost:3000/shipping/' + cep)
+        .then((data) => {
+            if (data.ok) {
+                return data.json();
+            }
+            throw data.statusText;
+        })
+        .then((data) => {
+            swal('Flete', `El flete es: US$${data.value.toFixed(2)}`, 'exito');
+        })
+        .catch((err) => {
+            swal('Error', 'Error al consultar flete', 'error');
+            console.error(err);
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const books = document.querySelector('.books');
+
+    fetch('http://localhost:3000/products')
+        .then((data) => {
+            if (data.ok) {
+                return data.json();
+            }
+            throw data.statusText;
+        })
+        .then((data) => {
+            if (data) {
+                data.forEach((book) => {
+                    books.appendChild(newBook(book));
+                });
+
+                document.querySelectorAll('.button-shipping').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        const id = e.target.getAttribute('data-id');
+                        const cep = document.querySelector(`.book[data-id="${id}"] input`).value;
+                        calculateShipping(id, cep);
+                    });
+                });
+
+                document.querySelectorAll('.button-buy').forEach((btn) => {
+                    btn.addEventListener('click', (e) => {
+                        swal('Compra de libro', 'Su compra fue realizada con exito', 'exito');
+                    });
+                });
+            }
+        })
+        .catch((err) => {
+            swal('Error', 'Error al listar los productos', 'error');
+            console.error(err);
+        });
+});
